6f8637870cdc183aff1793b5fcff1468a3d6535c
Eduardo18-06 <jorgetasiguano38@gmail.com>
2025-04-25 12:21:05 -0500

    Primer cambio, clase estudiante

--- src/ettallergit/ETTallerGit.java
+++ src/ettallergit/ETTallerGit.java
@@ -15,6 +15,7 @@ public class ETTallerGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       System.out.println("Clase estudiante");
     }
     
 }
